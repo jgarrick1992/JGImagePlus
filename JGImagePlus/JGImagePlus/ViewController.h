60fc5ec6a0dd23f0e0437828193e278cf10fe39f
Ji Fu <ji.fu@icloud.com>
2015-12-30 15:28:20 +0800

    Initial revision

---
+++ JGImagePlus/JGImagePlus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JGImagePlus
+//
+//  Created by Ji Fu on 12/29/15.
+//  Copyright © 2015 Ji Fu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JGImagePlus/JGImagePlus/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JGImagePlus
+//
+//  Created by Ji Fu on 12/29/15.
+//  Copyright © 2015 Ji Fu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
